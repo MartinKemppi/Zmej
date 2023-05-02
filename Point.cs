37b27a60c274af493c4751769c70b46a7093e49f
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-05-02 10:25:44 +0300

    Loeng 3 -> Loeng 4
    Vahetatud parametrid ning Point mooduli parameetrite lisamine

--- Point.cs
+++ Point.cs
@@ -12,6 +12,14 @@ namespace Zmej
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
