7ea7682dfabcb82182308aedcd62a908f5f6f6d0
MartinKemppi <120181210+MartinKemppi@users.noreply.github.com>
2023-05-02 10:19:11 +0300

    Loeng 2 -> Loeng 3
    Teksti sisaldava osa vahetatud

---
+++ Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zmej
+{
+    internal class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
